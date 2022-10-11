78ae503e7ce766b27c61a427f48b058cfe397922
mina0110 <minawati10@gmail.com>
2022-10-11 15:22:29 +0700

    merge LevelViewController into ChooseScenarioController

--- Komka/ChooseScenario/View/ChooseScenarioController.swift
+++ Komka/ChooseScenario/View/ChooseScenarioController.swift
@@ -9,6 +9,28 @@ import UIKit
 
 class ChooseScenarioController: UIViewController {
     
+    private lazy var btnMudah = Button(style: .active, title: "Mudah")
+    private lazy var btnSedang = Button(style: .idle, title: "Sedang")
+    private lazy var btnSusah = Button(style: .idle, title: "Susah")
+    
+    private lazy var stackView : UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [btnMudah, btnSedang, btnSusah])
+        stackView.axis = .horizontal
+        stackView.distribution = .equalCentering
+        stackView.spacing = 18
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var rewardButton : UIButton = {
+        let rewardBtn = UIButton()
+        rewardBtn.setBackgroundImage(UIImage(named: "RewardBtn.png"), for: .normal)
+        rewardBtn.translatesAutoresizingMaskIntoConstraints = false
+        rewardBtn.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        rewardBtn.heightAnchor.constraint(equalToConstant: 89).isActive = true
+        return rewardBtn
+    }()
+    
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
@@ -47,8 +69,21 @@ class ChooseScenarioController: UIViewController {
         navigationItem.hidesBackButton = true
         
         assignbackground()
+        view.addSubview(stackView)
+        view.addSubview(rewardButton)
+        setupAutoLayout()
         view.addSubview(collectionView)
         setCollectionView()
+    }
+    
+    func setupAutoLayout() {
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo:view.safeAreaLayoutGuide.topAnchor, constant: 164),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            rewardButton.topAnchor.constraint(equalTo:view.safeAreaLayoutGuide.topAnchor, constant: 59),
+            rewardButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -27)
+        ])
     }
 }
 
