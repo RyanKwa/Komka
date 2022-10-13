1ed02989eeb730f2bf81f912060742ce07876ff8
Ryan Vieri Kwa <ryan.kwa24@gmail.com>
2022-10-13 23:53:55 +0700

    Add function to add overlay in image

---
+++ Komka/Utilities/Extension/UIImageView+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  UIImageView+Ext.swift
+//  Komka
+//
+//  Created by Minawati on 13/10/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    
+    func addOverlay() {
+        let overlay: UIView = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.height))
+        overlay.backgroundColor = UIColor(white: 1, alpha: 0.7)
+        self.addSubview(overlay)
+    }
+}
