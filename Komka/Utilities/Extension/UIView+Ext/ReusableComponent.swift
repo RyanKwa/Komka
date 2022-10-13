e14ff6d00ee876fddb1a2ff7ed844bc6d9b45a7d
Ryan Vieri Kwa <ryan.kwa24@gmail.com>
2022-10-13 23:54:52 +0700

    Add reusable functions to extension

--- Komka/Utilities/Extension/UIView+Ext/ReusableComponent.swift
+++ Komka/Utilities/Extension/UIView+Ext/ReusableComponent.swift
@@ -9,13 +9,30 @@ import Foundation
 import UIKit
 
 extension UIView {
-    
+
+    @available(*, deprecated, renamed: "setImage()")
     static func setBackgroundImage() -> UIImageView {
         let backgroundImg = UIImageView(frame: UIScreen.main.bounds)
         backgroundImg.contentMode = .scaleToFill
         backgroundImg.image = #imageLiteral(resourceName: "bg")
         
         return backgroundImg
+    }
+
+    static func setImageView(imageName: String, contentMode: UIView.ContentMode? = .scaleToFill , clipsToBound: Bool? = false) -> UIImageView {
+        let imageView = UIImageView(frame: UIScreen.main.bounds)
+        imageView.image = UIImage(named: imageName)
+        imageView.contentMode = contentMode ?? .scaleToFill
+        imageView.clipsToBounds = clipsToBound ?? false
+        return imageView
+    }
+    
+    static func createImageIconBtn(title: String, imgTitle: String) -> UIButton {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle(title, for: .normal)
+        button.setImage(UIImage(named: imgTitle), for: .normal)
+        return button
     }
     
     static func createLabel(text: String,
