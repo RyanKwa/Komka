b1b8d501d5d193840d16e51c972cc2913c6bcd86
Ryan Vieri Kwa <ryan.kwa24@gmail.com>
2022-10-13 12:49:38 +0700

    Add models for cloudkit

---
+++ Komka/Models/WordImitation.swift
@@ -0,0 +1,14 @@
+//
+//  WordImitation.swift
+//  Komka
+//
+//  Created by Ryan Vieri Kwa on 13/10/22.
+//
+
+import CloudKit
+import Foundation
+
+struct WordImitation {
+    let id: CKRecord.ID?
+    let title: String?
+}
