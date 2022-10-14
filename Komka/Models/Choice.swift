2ff4c52166d6e0f6c10f103733f8c8ad9a3a41f0
Evelin Evelin <evelintungtung@gmail.com>
2022-10-14 11:58:57 +0700

    Add Model

---
+++ Komka/Models/Choice.swift
@@ -0,0 +1,14 @@
+//
+//  Choice.swift
+//  Komka
+//
+//  Created by Ryan Vieri Kwa on 13/10/22.
+//
+
+import CloudKit
+import Foundation
+
+struct Choice {
+    let id: CKRecord.ID?
+    let title: String?
+}
