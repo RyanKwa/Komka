20b341abc278fad2e419a1ce4b55a0fd81c2c7fe
mina0110 <minawati10@gmail.com>
2022-10-17 13:54:28 +0700

    rename UIImage overlay function

--- Komka/Utilities/Extension/UIImageView+Ext.swift
+++ Komka/Utilities/Extension/UIImageView+Ext.swift
@@ -10,7 +10,7 @@ import UIKit
 
 extension UIImageView {
     
-    func addOverlay() {
+    func addWhiteOverlay() {
         let overlay: UIView = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.height))
         overlay.backgroundColor = UIColor(white: 1, alpha: 0.7)
         self.addSubview(overlay)
