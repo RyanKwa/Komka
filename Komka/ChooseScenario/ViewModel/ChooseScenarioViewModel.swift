c14acf8717c8c6f2253e211587598c1adaa5c4f6
Evelin Evelin <evelintungtung@gmail.com>
2022-10-27 11:37:46 +0700

    Update fetch data using RxSwift

--- Komka/ChooseScenario/ViewModel/ChooseScenarioViewModel.swift
+++ Komka/ChooseScenario/ViewModel/ChooseScenarioViewModel.swift
@@ -8,18 +8,31 @@
 import Foundation
 import CloudKit
 import UIKit
+import RxSwift
 
 class ChooseScenarioViewModel {
     var scenarioDAO = ScenarioDAO.instance
     
-    @Published var scenarios: [Scenario] = []
-    @Published var assets: [ContentAsset] = []
+    var scenarios: [Scenario] = []
+    var assets: [ContentAsset] = []
+        
+    var scenariosPublisher = PublishSubject<[Scenario]>()
+    var assetsPublisher = PublishSubject<[ContentAsset]>()
+    
+    var bag = DisposeBag()
+
     
     func fetchScenario(){
         scenarioDAO.fetchScenarioData()
-        DispatchQueue.main.asyncAfter(deadline: .now()+2){
-            self.scenarios = self.scenarioDAO.scenarios
+        scenarioDAO.fetchAssetData()
+
+        scenarioDAO.assetsPublisher.subscribe(onCompleted: {
             self.assets = self.scenarioDAO.assets
-        }
+            self.scenarios = self.scenarioDAO.scenarios
+
+            self.assetsPublisher.onNext(self.assets)
+            self.assetsPublisher.onCompleted()
+        }).disposed(by: bag)
+        
     }
 }
