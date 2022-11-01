dfb6cfbec0be956ece1d7116449538786358d166
mina0110 <minawati10@gmail.com>
2022-11-01 19:34:09 +0700

    add MultipleChoicePart & Choices enum

---
+++ Komka/MultipleChoice/ViewModel/Choices.swift
@@ -0,0 +1,13 @@
+//
+//  Choices.swift
+//  Komka
+//
+//  Created by Minawati on 01/11/22.
+//
+
+import Foundation
+
+enum Choices: String, CaseIterable {
+    case leftChoice
+    case rightChoice
+}
---
+++ Komka/MultipleChoice/ViewModel/MultipleChoicePart.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleChoicePart.swift
+//  Komka
+//
+//  Created by Minawati on 01/11/22.
+//
+
+import Foundation
+
+enum MultipleChoicePart: String, CaseIterable {
+    case scenarioCover
+    case multipleChoiceCharacter
+    case leftChoice
+    case rightChoice
+    case wrongChoice
+    case correctChoice
+}
