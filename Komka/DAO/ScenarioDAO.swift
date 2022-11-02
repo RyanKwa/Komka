83d4f2fc1a3218940c03f1185e90db2643ed4ae9
Evelin Evelin <evelintungtung@gmail.com>
2022-11-02 16:17:32 +0700

    Add Error Handling

--- Komka/DAO/ScenarioDAO.swift
+++ Komka/DAO/ScenarioDAO.swift
@@ -35,19 +35,27 @@ class ScenarioDAO{
                     let scenarioReference = record["scenario"] as? CKRecord.Reference,
                     let imageStep = record["step"] as? String,
                     let scenarioImage = record["image"] as? CKAsset
-                else { return }
+                else {
+                    self.assetsPublisher.onError(FetchError.missingData(recordType: RecordType.Asset))
+                    return
+                }
                 
                 self.assets.append(ContentAsset(id: returnedRecordID, title: imageTitle, gender: imageGender, scenario: scenarioReference, step: imageStep, image: scenarioImage))
                 
                 
-            case .failure(let error):
-                print("Error recordMatchedBlock: \(error)")
+            case .failure(_):
+                self.assetsPublisher.onError(FetchError.failedQuery(recordType: RecordType.Asset))
             }
         }
         
-        queryOperationAsset.queryResultBlock = { _ in
-            self.assetsPublisher.onNext(self.assets)
-            self.assetsPublisher.onCompleted()
+        queryOperationAsset.queryResultBlock = { result in
+            switch result {
+            case .success(_):
+                self.assetsPublisher.onNext(self.assets)
+                self.assetsPublisher.onCompleted()
+            case.failure(_):
+                self.assetsPublisher.onError(FetchError.failedQuery(recordType: RecordType.Asset))
+            }
         }
         self.ckHelper.db.add(queryOperationAsset)
     }
@@ -66,18 +74,26 @@ class ScenarioDAO{
                     let scenarioStatus = record["isCompleted"] as? Bool,
                     let scenarioSentence = record["sentence"] as? String,
                     let scenarioLevel = record["level"] as? CKRecord.Reference
-                else { return }
+                else {
+                    self.scenariosPublisher.onError(FetchError.missingData(recordType: RecordType.Scenario))
+                    return
+                }
                 
                 self.scenarios.append(Scenario(id: returnedRecordID, title: scenarioTitle, isCompleted: scenarioStatus, sentence: scenarioSentence, level: scenarioLevel, reward: nil, multipleChoice: nil, wordImitations: []))
                 
-            case .failure(let error):
-                print("Error recordMatchedBlock: \(error)")
+            case .failure(_):
+                self.scenariosPublisher.onError(FetchError.failedQuery(recordType: RecordType.Scenario))
             }
         }
         
-        queryOperationScenario.queryResultBlock = { _ in
-            self.scenariosPublisher.onNext(self.scenarios)
-            self.scenariosPublisher.onCompleted()
+        queryOperationScenario.queryResultBlock = { result in
+            switch result {
+            case .success(_):
+                self.scenariosPublisher.onNext(self.scenarios)
+                self.scenariosPublisher.onCompleted()
+            case.failure(_):
+                self.scenariosPublisher.onError(FetchError.failedQuery(recordType: RecordType.Scenario))
+            }
         }
         self.ckHelper.db.add(queryOperationScenario)
         
