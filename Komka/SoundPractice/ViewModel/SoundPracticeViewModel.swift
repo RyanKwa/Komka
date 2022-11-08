fcd42a388ebd561ef089e7937cf2dbc7b942c7b9
Evelin Evelin <evelintungtung@gmail.com>
2022-11-08 20:57:47 +0700

    Create audio manager for sound practice

--- Komka/SoundPractice/ViewModel/SoundPracticeViewModel.swift
+++ Komka/SoundPractice/ViewModel/SoundPracticeViewModel.swift
@@ -86,4 +86,54 @@ class SoundPracticeViewModel {
         TextToSpeechService.shared.stopSpeech()
         TextToSpeechService.shared.startSpeech(queue)
     }
+    
+    lazy var queueWordCounter: Int = 1
+    private (set) lazy var progressFrom = setProgressFrom()
+    private (set) lazy var progressTo = setProgressTo()
+    
+    var audioStreamManager = AudioStreamManager()
+    
+    var progressPublisher = BehaviorSubject(value: 0.0)
+    var audioPublisher = PublishSubject<Double>()
+        
+    lazy var result = Double()
+    
+    func setProgressTo() -> Double{
+        lazy var tempVar = 0.1
+//        audioPublisher.subscribe(onNext: { [self] confidence in
+            result = (tempVar * 35.0)/100.0
+            
+            if result <= 0.0 {
+                result = 0.0
+            }
+            else if result < 0.15 {
+                result = 0.15
+            }
+//        }).disposed(by: disposeBag)
+
+        progressPublisher.onNext(result)
+        return result
+    }
+    
+    func setProgressFrom() -> Double{
+        progressPublisher.subscribe(onNext: { [self] result in
+            progressFrom = result
+            print(progressFrom)
+            
+        }).disposed(by: disposeBag)
+        
+        return progressFrom
+    }
+    
+    func startSoundPractice(){
+        audioStreamManager.startLiveAudio()
+        
+        //logic dmn, bakal kirim confidence terus menerus (mgkin bisa via publisher, utk trigger si setProgressTo)?
+        var confidence = 0.2
+        audioPublisher.onNext(confidence)
+    }
+    
+    func stopSoundPractice(){
+        audioStreamManager.stopLiveAudio()
+    }
 }
