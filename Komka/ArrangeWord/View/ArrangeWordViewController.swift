c445c7f501a4c773588d42e4f38e3b07b35e35cb
mina0110 <minawati10@gmail.com>
2022-11-09 22:32:45 +0700

    fix sound effect transition from arrange word to completion page

--- Komka/ArrangeWord/View/ArrangeWordViewController.swift
+++ Komka/ArrangeWord/View/ArrangeWordViewController.swift
@@ -187,10 +187,7 @@ extension ArrangeWordViewController: UICollectionViewDelegate, UICollectionViewD
                 self?.showResultBorder(slotCell: selectedSlot, wordCell: currentSelectedWordCell, withResult: isWordPlacementCorrect)
             })
             if totalCorrectWord == correctSentencesInOrder.count {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                    SoundEffectService.shared.playSoundEffect(.CompletionPage)
-                }
-                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
                     self.navigationController?.pushViewController(CompletionPageViewController(), animated: true)
                 }
             }
